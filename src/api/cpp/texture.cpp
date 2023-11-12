15603ad72593c233b9c44817ec1f3d3fcf710d1c
qrst0 <66962233+qrst0@users.noreply.github.com>
2023-11-12 22:14:44 +0700

    update texture.cpp dan cmptexture.cpp

--- src/api/cpp/texture.cpp
+++ src/api/cpp/texture.cpp
@@ -1,6 +1,6 @@
 #pragma GCC optimize("03")
 #include <bits/stdc++.h>
-#include "lib/json/single_include/nlohmann/json.hpp"
+#include "json\single_include\nlohmann\json.hpp"
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "lib/stb_image.h"
@@ -14,22 +14,41 @@ using json = nlohmann::json;
 namespace fs = std::filesystem;
 
 const int channel = 256;
+const int numThreads = 6;
 const double pi = acos(-1.0);
 const vector<int> dxs = {0, -1, -1, -1, 0};
 const vector<int> dys = {1, 1, 0, -1, -1};
 const vector<int> distances = {1};
-const string abspath = "../public/images/dataset";
-const string preProcessJsonFileName = "texture.json";
+const string abspath = "dataset";
+const string jsonFileName = "texture.json";
 
-double hist[channel][channel];
+vector< pair<string, string> > filepaths;
 
-vector< double > res;
+std::mutex m;
 
-void img_to_texture_vector(string path){
+namespace indexed {
+    struct index{
+        string name;
+        vector< double > vec;
+    };
+    void to_json(json& j, const index& p) {
+        j = json{ {"name", p.name}, {"vector", p.vec}};
+    }
+    void from_json(const json& j, index& p) {
+        j.at("name").get_to(p.name);
+        j.at("vector").get_to(p.vec);
+    }
+}
+
+vector< indexed::index > res;
+
+void img_to_texture_vector(string path, string name){
+    double hist[channel][channel];
     int width, height, ori, desired = 3;
     unsigned char *img = stbi_load(path.c_str(), &width, &height, &ori, desired);
     if(img == NULL){
-        return;
+        cout << "ERROR\n";
+        exit(1);
     }
     int img_size = width * height * desired;
     int g_img_size = width * height;
@@ -44,6 +63,7 @@ void img_to_texture_vector(string path){
         unsigned char conv = valY;
         *pg = conv;
     }
+    vector < double > done;
     for(auto &d: distances){
         assert(dxs.size() == dys.size());
         for(int i = 0; i < (int)dxs.size(); ++i){
@@ -77,24 +97,52 @@ void img_to_texture_vector(string path){
                 for (int j = 0; j < channel; ++j) {
                     contrast += hist[i][j] * (i - j) * (i - j);
                     homogeneity += (hist[i][j] / (1 + (i - j) * (i - j)));
-                    if (hist[i][j] != 0.0) entropy += hist[i][j] * -log(hist[i][j]);
+                    if (hist[i][j] != 0.0) entropy -= hist[i][j] * log(hist[i][j]);
                 }
             }
-            res.push_back(contrast);
-            res.push_back(homogeneity);
-            res.push_back(entropy);
+            done.push_back(contrast);
+            done.push_back(homogeneity);
+            done.push_back(entropy);
         }
+    }
+    indexed::index objJson;
+    objJson.name = name;
+    objJson.vec = done;
+    m.lock();
+    res.push_back(objJson);
+    m.unlock();
+}
+
+void procUnitThread(int stInd, int endInd){
+    for(; stInd <= endInd; ++stInd){
+        img_to_texture_vector(filepaths[stInd].first, filepaths[stInd].second);
     }
 }
 
 int main(){
     auto beg = high_resolution_clock::now();
     for(const auto &entry: fs::directory_iterator(abspath)){
-        img_to_texture_vector(entry.path().string());
+        filepaths.push_back({entry.path().string(), entry.path().filename().string()});
     }
+    vector<std::thread> threads;
+    int numFiles = filepaths.size();
+    vector<pair<int, int>> intervals;
+    for(int i = 0, sz = numThreads, st = 0; i < numThreads; ++i, --sz){
+        int szcur = numFiles / sz;
+        intervals.push_back({st, st + szcur - 1});
+        st += szcur;
+        numFiles -= szcur;
+    }
+    for(auto &p: intervals){
+        threads.emplace_back(procUnitThread, p.first, p.second);
+    }
+    for(auto &thread: threads){
+        thread.join();
+    }
+    assert(res.size() == filepaths.size());
     json j;
     j = res;
-    ofstream file(preProcessJsonFileName);
+    ofstream file(jsonFileName);
     file << j; 
     auto en = high_resolution_clock::now();
     auto dur = duration_cast<milliseconds>(en - beg);
