323f02857917247bfcf1a5c2b5d88ca886cb0624
Berto Richardo Togatorop <bertotogatorop27@gmail.com>
2023-11-15 04:27:15 +0700

    feat: Adding dependencies and configuration for tailwind

---
+++ src/client/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        accentColor: "#802BB1",
+        backColor1: "#2D283E",
+        backColor2: "#564F6F",
+        backColor3: "#4C495D",
+        textColor: "#D1D7E0",
+      },
+    },
+  },
+  plugins: [],
+};
