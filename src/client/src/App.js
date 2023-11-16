4ca862929112b1ebe55dc47cec8997c58dd95503
ninoaddict <adrilbless37@gmail.com>
2023-11-16 13:12:49 +0700

    feat: update header file and assets

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -8,7 +8,6 @@ import Description from "./components/Description";
 const App = () => {
   return (
     <Router>
-      <Header />
       <Routes>
         <Route path="/description" element={<Description />} />
         <Route path="/about" element={<About />} />
@@ -17,3 +16,5 @@ const App = () => {
     </Router>
   );
 };
+
+export default App;
